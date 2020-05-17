b846420e507a5fe27b081fa40553e25d17e79c02
MapManagement <fortnitetest@mail.de>
2020-05-17 20:49:02 +0200

    created simple game menu

--- game.py
+++ game.py
@@ -1,31 +1,43 @@
 import arcade
+import os
 
-# constants for the screen size (obviously)
+# constants for the screen
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+SCREEN_TITLE = "MapGame"
+
+SPRITE_SCALING_PLAYER = 0.1
 
 
-class MapGame(arcade.Window):
+class MenuView(arcade.View):
 
-    def __init__(self, width, height):
-        super().__init__(width, height)
-        arcade.set_background_color(arcade.color.AZURE)
+    def on_show(self):
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("Game Menu", SCREEN_WIDTH/2, SCREEN_HEIGHT/2,
+                         arcade.color.BLACK, 50, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
+
+
+class GameView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
 
     def setup(self):
         pass
 
-    def on_draw(self):
-        arcade.start_render()
-        # things that need to be rendered
-
-    def update(self, delta_time: float):
-        # things that happen during the game runs
-        pass
-
 
 def main():
-    game = MapGame(SCREEN_WIDTH, SCREEN_HEIGHT)
-    game.setup()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "MapGame")
+    menu_view = MenuView()
+    window.show_view(menu_view)
     arcade.run()
 
 
